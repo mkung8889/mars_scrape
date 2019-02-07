752f38c1bd0d96263fad4c242dff383c5eddbdf6
Michael <m8464k@gmail.com>
2019-02-06 22:15:29 -0600

    upload all mars_scrape files

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+import scrape_mars
+
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def mars():
+       mars_data= mongo.db.collection.find_one()
+       return render_template("index_copy.html",mars=mars_data)
+
+@app.route("/scrape")
+def run_scrape():
+    mars = mongo.db.mars
+    mars_info = scrape_mars.scrape()
+    mongo.db.collection.update({}, mars_info, upsert=True)
+    return redirect("/", code=302)
+
+if __name__ == "__main__":
+    app.run(debug=True)
