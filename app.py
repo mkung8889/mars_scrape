b85e0eb41a0b2d612264f29966bcee24fbb30f4e
Michael <m8464k@gmail.com>
2019-02-07 18:35:47 -0600

    update collection name

--- app.py
+++ app.py
@@ -13,14 +13,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def mars():
-       mars_data= mongo.db.collection.find_one()
+       mars_data= mongo.db.mars_data.find_one()
        return render_template("index.html",mars=mars_data)
 
 @app.route("/scrape")
 def run_scrape():
     mars = mongo.db.mars
     mars_info = scrape_mars.scrape()
-    mongo.db.collection.update({}, mars_info, upsert=True)
+    mongo.db.mars_data.update({}, mars_info, upsert=True)
     return redirect("/", code=302)
 
 if __name__ == "__main__":
